8cff392e60d7268b54f60a2dee62b3ba79e2e749
Shahu S Ronghe <sronghe1@binghamton.edu>
2021-11-01 11:04:19 -0400

    11th

---
+++ problems/11. Container WIth Most Water/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int maxarea = 0, left = 0, right = height.length - 1;
+        while (left < right) {
+            maxarea = Math.max(maxarea, Math.min(height[left], height[right]) * (right - left));
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+        return maxarea;
+    }
+}
